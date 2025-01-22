3c5ad0152847b1ff1c79ca95efd8752dda795b0e
artbal89 <balingitartchristian@gmail.com>
2025-01-22 18:06:15 +0800

    Add files via upload

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$subj = "New message from the site 'alb'"; //letter subject
+	$to = 'balingitartchristian@gmail.com'; // Enter Your E-mail
+	$from = 'no-reply.com'; // Admin e-mail
+	$fromName = 'alb'; // Your company name
 	$charset = 'UTF-8';
 ?>
